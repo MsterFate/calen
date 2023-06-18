4f93e1d5a33e5292589899e297d74e5abbcfb845
MsterFate <wadihe100@gmail.com>
2023-06-18 04:01:59 -0400

    Se realizo un cambio en el proyecto

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000',
-     'https://calen-yq2c.vercel.app/'
+     'https://calen-yq2c.vercel.app'
 ]
